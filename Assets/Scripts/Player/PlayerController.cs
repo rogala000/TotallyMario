817bf69f97f171fa8e1ea67188ff4136417e8d47
rogala <rogala000@gmail.com>
2019-07-14 19:34:42 +0200

    player movement and prototype level for testing it

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float Speed = 1;
+    [SerializeField] private float jumpPower = 1;
+    [SerializeField] private bl_Joystick Joystick;
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private Button jumpButton;
+    [SerializeField] private Camera camera;
+    private void Start()
+    {
+        jumpButton.onClick.AddListener(Jump);
+    }
+
+    void FixedUpdate()
+    {
+        float h = Joystick.Horizontal;
+        camera.transform.position = new Vector3(transform.position.x, camera.transform.position.y, camera.transform.position.z);
+        Vector3 translate = (new Vector3(h, 0, 0) * Time.fixedDeltaTime) * Speed;
+        transform.Translate(translate);
+    }
+
+    private void Jump()
+    {
+        if(rigidbody.velocity.y == 0)
+        {
+            rigidbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+        }
+    }
+
+}
