b9ac020d67a79869bb92227f82c74787768807af
rogala <rogala000@gmail.com>
2019-07-16 15:58:38 +0200

    added background music

---
+++ Assets/Scripts/Misc/BackgroundMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))] 
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> songs;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+       // audioSource.loop = true;
+        StartCoroutine(playFirstSong());
+        DontDestroyOnLoad(this);
+    }
+
+    IEnumerator playFirstSong()
+    {
+        audioSource.clip = songs[0];
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        StartCoroutine(playRandomSong ());
+    }
+
+
+    IEnumerator playRandomSong()
+    {
+        audioSource.clip = songs[Random.Range(0, songs.Count)];
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        StartCoroutine(playRandomSong());
+
+    }
+}
