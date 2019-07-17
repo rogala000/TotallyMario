88175cdbbec8979bf7ce844f9195c7421b99e1e1
rogala <rogala000@gmail.com>
2019-07-17 19:59:34 +0200

    level 4

---
+++ Assets/Scripts/Obstacles/DissapearingGround.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DissapearingGround : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Obstacles/FallingSpikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour
+{
+
+    [SerializeField] Transform spikesTransform;
+    bool isTrapTriggered = false;
+    [SerializeField] private int speed = 1;
+    
+
+    void Update()
+    {
+        if(isTrapTriggered)
+        {
+            spikesTransform.Translate(Vector3.down * speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isTrapTriggered = true;
+    }
+}
